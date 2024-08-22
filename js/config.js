857e6d6d9d65da4808ecd7b224419ecf0d91bf60
MEHSOL <mehdisoltana@MacBook-Pro-de-Mehdi.local>
2024-08-22 17:05:53 +0200

    test

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-north-1_ALPR3YJ9r', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5mj35naotk5lq6768hm0ohf5up', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
