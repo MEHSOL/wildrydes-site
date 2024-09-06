8ae4e2f85c6c976430353aa58a470f9b570b224f
MEHSOL <mehdisoltana@mbp-de-mehdi.home>
2024-09-06 22:36:37 +0200

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_ALPR3YJ9r', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5mj35naotk5lq6768hm0ohf5up', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-north-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_iKyvIQKIH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '40gbsr4r01kk0f4j39o9a85dic', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
